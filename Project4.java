dfc12315068a993b7f86cfa9da29d1131ae865ab
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2023-05-27 10:55:24 -0400

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,6 @@
+
+public class Project4 {
+    public static void main(String[] args) {
+        RomanNumeralGUI rmnGUI = new RomanNumeralGUI("Roman to Arabic GUI"); // creates GUI
+    }// main function
+}//class
